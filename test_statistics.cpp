62f609ad256023ead9828e95d338383d5e25707d
artemka332 <artemkasashin@yandex.ru>
2025-10-15 16:35:41 +0300

    добавили тест и изменили под него файл cmake

---
+++ test_statistics.cpp
@@ -0,0 +1,17 @@
+#include "statistics.h"
+#include <cassert>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    vector<double> nums = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    assert(Statistics::sum(nums) == 15.0);
+    assert(Statistics::min(nums) == 1.0);
+    assert(Statistics::max(nums) == 5.0);
+
+    cout << "All tests passed!"<<endl;
+    return 0;
+}
